c1929af122c4b423daae054b471c6573aabd9669
Takuma Kira <takuma@kirakiraworx.com>
2024-12-25 15:44:34 +0900

    [dev]: Edit exercises on workout

--- fitness_workout_tracker_api/workouts/views.py
+++ fitness_workout_tracker_api/workouts/views.py
@@ -32,9 +32,19 @@ class WorkoutViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         return Workout.objects.filter(user=self.request.user)
 
-class WorkoutExerciseViewSet(viewsets.GenericViewSet):
-    serializer_class = AddExerciseToWorkoutSerializer
+class WorkoutExerciseViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
+
+    def get_serializer_class(self):
+        if self.action in ['create', 'update', 'partial_update']:
+            return AddExerciseToWorkoutSerializer
+        return WorkoutExerciseSerializer
+
+    def get_queryset(self):
+        return WorkoutExercise.objects.filter(
+            workout_id=self.kwargs['workout_pk'],
+            workout__user=self.request.user
+        )
 
     def create(self, request, workout_pk=None):
         """
@@ -59,3 +69,39 @@ class WorkoutExerciseViewSet(viewsets.GenericViewSet):
                 status=status.HTTP_201_CREATED
             )
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def update(self, request, workout_pk=None, pk=None):
+        """
+        Update an exercise in the workout
+        """
+        workout_exercise = self.get_object()
+        serializer = self.get_serializer(data=request.data)
+        
+        if serializer.is_valid():
+            exercise = Exercise.objects.get(id=serializer.validated_data['exercise_id'])
+            workout_exercise.exercise = exercise
+            workout_exercise.sets = serializer.validated_data['sets']
+            workout_exercise.reps = serializer.validated_data['reps']
+            workout_exercise.weight = serializer.validated_data.get('weight')
+            workout_exercise.notes = serializer.validated_data.get('notes', '')
+            workout_exercise.order = serializer.validated_data.get('order', 0)
+            workout_exercise.save()
+            
+            return Response(
+                WorkoutExerciseSerializer(workout_exercise).data,
+                status=status.HTTP_200_OK
+            )
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def destroy(self, request, workout_pk=None, pk=None):
+        """
+        Remove an exercise from the workout
+        """
+        workout_exercise = get_object_or_404(
+            WorkoutExercise,
+            workout_id=workout_pk,
+            id=pk,
+            workout__user=request.user
+        )
+        workout_exercise.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
